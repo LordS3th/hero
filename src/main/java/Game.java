bdaa4bea051b40b9adacfa183f882c9b2f580c36
LordS3th <84471154+LordS3th@users.noreply.github.com>
2021-11-16 09:38:21 +0000

    7 Done

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,17 +1,22 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import javax.swing.plaf.basic.BasicArrowButton;
 import java.io.IOException;
 
 public class Game {
+    private int x = 10;
+    private int y = 10;
     Screen screen;
-    public static void main(String[] args) {
+    public Game() {
         try {
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
+            screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null);   // we don't need a cursor
             screen.startScreen();             // screens must be started
             screen.doResizeIfNecessary();     // resize screen if necessary
@@ -21,7 +26,7 @@ public class Game {
     }
         private void draw () {
             screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
             try {
                 screen.refresh();
             } catch (IOException e) {        //Need to understand exceptions
@@ -29,8 +34,31 @@ public class Game {
             }
         }
         public void run () {
-            draw();
+            while(1==1) {
+                draw();
+                try {
+                    KeyStroke key = screen.readInput();
+                    if (key.getKeyType()==KeyType.EOF) break;
+                    if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') screen.close();
+                    switch (key.getKeyType()) {
+                        case ArrowUp:
+                            y -= 1;
+                            break;
 
+                        case ArrowDown:
+                            y += 1;
+                            break;
+
+                        case ArrowLeft:
+                            x -= 1;
+                            break;
+                        case ArrowRight:
+                            x += 1;
+                            break;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
     }
-    
 }
---
+++ src/main/java/Hero.java
@@ -0,0 +1,5 @@
+public class Hero {
+    private int x;
+    private int y;
+    
+}
